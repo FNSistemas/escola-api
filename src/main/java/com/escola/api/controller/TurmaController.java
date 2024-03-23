34ec79638a4d00e2cc67a7861933e5a0544b1fc2
FNSistemas <138892642+FNSistemas@users.noreply.github.com>
2024-03-22 21:08:59 -0300

    Criacao do PUT nas Turmas

--- src/main/java/com/escola/api/controller/TurmaController.java
+++ src/main/java/com/escola/api/controller/TurmaController.java
@@ -3,6 +3,7 @@ package com.escola.api.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,8 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
+import com.escola.api.model.Aluno;
 import com.escola.api.model.Turma;
 import com.escola.api.repository.TurmaRepository;
 
@@ -31,14 +35,50 @@ public class TurmaController {
 	  	return ResponseEntity.status(HttpStatus.OK).body(turmaRepository.findAll());
        }
    
+       @GetMapping("/{id}")
+	  public ResponseEntity<Optional<Turma>> obterTurmaPeloID(@PathVariable("id") long id)  {
+        Optional<Turma> optionaTurma = turmaRepository.findById((id));
+
+        if (optionaTurma.isEmpty()) {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).build();//
+        }  
+
+        return ResponseEntity.status(HttpStatus.OK).body(turmaRepository.findById(id));
+
+        }
+
+        @GetMapping("/nome{nome}")
+        public ResponseEntity<Optional<Turma>> obterTurmaPeloNome(@PathVariable("nome") String nome)  {
+           Optional<Turma> optionaTurma = turmaRepository.findByNome((nome));
+   
+           if (optionaTurma.isEmpty()) {
+              return ResponseEntity.status(HttpStatus.NOT_FOUND).build();//
+           }  
+   
+           return ResponseEntity.status(HttpStatus.OK).body(turmaRepository.findByNome(nome));//
+        }
 
 
+	
+   
+       @PutMapping("/{id}")
+       public ResponseEntity<Turma> atualizarTurma(@PathVariable("id") long id, @RequestBody Turma turma){
+          Optional<Turma> optionaTurma = turmaRepository.findById((id));
 
-       
+        if (optionaTurma.isEmpty()) {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).build();//
+        }
+        
+        Turma turmaExistente = optionaTurma.get();
 
+        turmaExistente.setNome((turma.getNome()));
+        turmaExistente.setCargaHoraria((turma.getCargaHoraria()));
+        turmaExistente.setDataInicio((turma.getDataInicio()));
+        turmaExistente.setDataTermino((turma.getDataTermino()));
 
+        return ResponseEntity.status(HttpStatus.OK).body(turmaRepository.save(turmaExistente));
 
-
+     }    
 
 
 
--- src/main/java/com/escola/api/repository/TurmaRepository.java
+++ src/main/java/com/escola/api/repository/TurmaRepository.java
@@ -1,11 +1,16 @@
 package com.escola.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.escola.api.model.Aluno;
 import com.escola.api.model.Turma;
 
 @Repository
 public interface TurmaRepository extends JpaRepository<Turma,Long>{
+
+    Optional<Turma> findByNome(String nome);
 
 }
