5cfcd809a253d34d892e6e6252e0cf1d323f9453
FNSistemas <138892642+FNSistemas@users.noreply.github.com>
2024-03-22 21:12:23 -0300

    Criacao do DELETE Turmas

--- src/main/java/com/escola/api/controller/TurmaController.java
+++ src/main/java/com/escola/api/controller/TurmaController.java
@@ -11,12 +11,12 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
-import com.escola.api.model.Aluno;
 import com.escola.api.model.Turma;
 import com.escola.api.repository.TurmaRepository;
 
@@ -80,6 +80,20 @@ public class TurmaController {
 
      }    
 
+
+     
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirTurma(@PathVariable("id") long id){
+       Optional<Turma> optionaTurma = turmaRepository.findById((id));
+
+       if (optionaTurma.isEmpty()) {
+          return ResponseEntity.status(HttpStatus.NOT_FOUND).build();//
+       }
+
+       turmaRepository.deleteById(id);
+
+       return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
 
        @Autowired
